d03e6b0fc0f6cf0aa7326a7f3c9cabd5b06f54a0
spaasie <68701772+spaasie@users.noreply.github.com>
2023-04-24 14:51:55 +0200

    add styling and tweak validate function

--- js/validate.js
+++ js/validate.js
@@ -1,7 +1,16 @@
 function validate() {
   let password = document.getElementById("password");
   let confirm = document.getElementById("confirm_password");
+  let confirmNotify = document.getElementById("confirm_pass_notify");
 
-  if (password.value == confirm.value) return;
-  else alert("password not the same");
+  if (password.value == confirm.value) {
+    confirmNotify.textContent = "";
+    confirmNotify.setAttribute("isvalid", "true");
+    // confirmNotify.setCustomValidity("");
+  }
+  else {
+    confirmNotify.textContent = "password not the same";
+    confirmNotify.setAttribute("isvalid", "false");
+    // confirmNotify.setCustomValidity("Password no match");
+  }
 }
